c988c55178bc743c94510445cd650e061268fc03
Amruth Lal <amruth27m@gmail.com>
2018-04-27 23:54:37 +0530

    Print name in Intellij welcome project

--- src/com/amruth/java/Main.java
+++ src/com/amruth/java/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Intellij Idea is awesome!");
+        String tempString = "Amruth";
+        System.out.println("Your name is "+tempString);
     }
 }
