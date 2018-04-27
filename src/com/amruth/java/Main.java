1fac606bbc1099e875c5a95aaf073bb68bd22a63
Amruth Lal <amruth27m@gmail.com>
2018-04-27 23:50:40 +0530

    Intellij idea test project

---
+++ src/com/amruth/java/Main.java
@@ -0,0 +1,9 @@
+package com.amruth.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Intellij Idea is awesome!");
+    }
+}
