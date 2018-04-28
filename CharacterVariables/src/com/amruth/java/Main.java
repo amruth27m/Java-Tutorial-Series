50c0ea43d95db225fd943546b15665821c595975
Amruth Lal <amruth27m@gmail.com>
2018-04-28 14:59:48 +0530

    Add string equals method and instanceof method

--- CharacterVariables/src/com/amruth/java/Main.java
+++ CharacterVariables/src/com/amruth/java/Main.java
@@ -10,5 +10,20 @@ public class Main {
         char c = '$';
         System.out.println(Character.toUpperCase(a));
 
+        String s = "Hello Idea!";
+        if(s instanceof java.lang.String) {
+            System.out.println("s is a string");
+        }
+
+        String s2 = "Hello Idea!";
+        if(s == s2){
+            System.out.println("Normal equality works");
+        }
+        else{
+            if(s.equals(s2)){
+                System.out.println("strings match using equals method");
+            }
+        }
+
     }
 }
