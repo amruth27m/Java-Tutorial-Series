0e97fe161d93d88fa5dce5dea8868acd385180b0
Amruth Lal <amruth27m@gmail.com>
2018-04-28 14:52:44 +0530

    Add Character helper class

---
+++ CharacterVariables/src/com/amruth/java/Main.java
@@ -0,0 +1,14 @@
+package com.amruth.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        char a = 'b';
+        char b = '1';
+        System.out.println("Chars " + a +b);
+        char c = '$';
+        System.out.println(Character.toUpperCase(a));
+
+    }
+}
